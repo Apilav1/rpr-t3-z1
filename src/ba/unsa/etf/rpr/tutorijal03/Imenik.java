5894a8e23606f4139a8422204c5da0844b8e34e3
Apilav1 <apilav1@etf.unsa.ba>
2018-11-01 10:36:56 +0100

    dodano naSlovo() Imenik

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -15,4 +15,21 @@ public class Imenik {
         }
         return null;
     }
+    String dajIme(TelefonskiBroj broj){
+        for (Map.Entry<String, TelefonskiBroj>o: mapa.entrySet()) {
+            if(o.getValue().equals(broj)){
+                return o.getKey();
+            }
+        }
+        return null;
+    }
+    String naSlovo(char s){
+        int i=1;
+        for (Map.Entry<String, TelefonskiBroj>o: mapa.entrySet()) {
+            if(o.getKey().charAt(0)=='s'){
+                String.format("%d . %s - %s", i, o.getKey(), o.getValue());
+            }
+        }
+        return null;
+    }
 }
