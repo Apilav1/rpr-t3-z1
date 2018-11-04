ea17a581a8439168e2e97a8e78320c8b07bec99e
Apilav1 <apilav1@etf.unsa.ba>
2018-11-04 09:26:43 +0100

    dodana klasa Program (nedovrsena)

---
+++ src/ba/unsa/etf/rpr/tutorijal03/Program.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+import java.util.*;
+
+public class Program {
+    public enum Grad{SARAJEVO, TUZLA, ZENICA, MOSTAR};
+    public static void main(String[] args) {
+        Imenik i = new Imenik();
+   //     i.dodaj("Adi", new FiksniBroj(Grad.SARAJEVO, "111-111"));
+    }
+}
